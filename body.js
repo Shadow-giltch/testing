c048e88f14d4bf6dff064c9ac7b8b3e6ef5aa2f9
Shadow-giltch <raghavcucumber@gmail.com>
2026-01-09 10:59:27 +0530

    2nd

---
+++ body.js
@@ -0,0 +1,16 @@
+const product={
+    fullNname:"Ball pen",
+    rating:4.1,
+    price:270,
+    isDeal:true
+}
+
+const instagramPost={
+    username:"shardakhapra",
+    isfollowed:true,
+    noOfposts:350,
+    noOffollowers:569000,
+    noOffollowing:4
+}
+
+console.log(instagramPost);
